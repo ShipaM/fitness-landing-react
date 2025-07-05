27a354a7f53937e3ef167bcb93f24e93345b2742
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2025-07-05 12:28:20 +0300

    feat(init-project): add fitness-landing-react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex flex-col gap-4 justify-center">
+      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <p className="font-montserrat">Hello</p>
+      <p className="font-dm-sans">Hello</p>
+    </div>
+  );
+}
+
+export default App;
