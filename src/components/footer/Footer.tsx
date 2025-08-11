0710eb79f347ed78a65463416a99dbd1c0f29b8d
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2025-08-11 20:17:05 +0300

    feat update import

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/Logo.png";
 import { footerLinks } from "@/utils/footerLinks";
 import { baseMotion, slideUp, withDelay } from "@/utils/motionPresets";
 import { motion } from "framer-motion";
