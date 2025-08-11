06e19de1abfc5d63d5237023045da2b6b9ed3ac7
Maksym Shypytsia <30442574+ShipaM@users.noreply.github.com>
2025-08-11 20:20:04 +0300

    feat update import

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/assets/logo.png";
+import Logo from "@/assets/Logo.png";
 import { ActionButton } from "../ActionButton";
 import { useEffect, useState } from "react";
 import { navLinks, type SectionId } from "@/types/type";
